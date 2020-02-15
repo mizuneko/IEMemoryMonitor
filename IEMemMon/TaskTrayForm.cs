09f874d81cc8a2d0a97b2c29df882d980eff8500
Kenji Tabata <retmiz@gmail.com>
2020-02-15 17:23:27 +0900

    メニューの生成をメソッド化

--- IEMemMon/TaskTrayForm.cs
+++ IEMemMon/TaskTrayForm.cs
@@ -10,8 +10,8 @@ namespace IEMemMon
         public CycleMeasurementMemory counter = new CycleMeasurementMemory();
         NotifyIcon TaskTrayIcon = new NotifyIcon();
         readonly long alertMaxSizeMB = 600;
-
         readonly long scale = 1000000;  // MegaByte
+        readonly string ProcessName = "iexplore";
 
         public TaskTrayForm()
         {
@@ -25,23 +25,15 @@ namespace IEMemMon
             TaskTrayIcon.Icon = Properties.Resources.NormalIcon;
             TaskTrayIcon.Visible = true;
 
-            ContextMenuStrip menu = new ContextMenuStrip();
-
-            ToolStripMenuItem closeItem = new ToolStripMenuItem();
-            closeItem.Text = "&終了";
-            closeItem.Click += CloseItem_Click;
-            menu.Items.Add(closeItem);
-
-            TaskTrayIcon.ContextMenuStrip = menu;
-
+            setMenuItem();
+            
             var counter = new CycleMeasurementMemory();
-            var processName = "iexplore";
             Process[] ps = null;
 
             counter.Elapsed = (object o, MemoryEventArgs m) =>
             {
                 // ツールヒントにはSum値とMax値それぞれ表示するが、アラートの閾値判定はMax値を利用する
-                ps = Process.GetProcessesByName(processName);
+                ps = Process.GetProcessesByName(ProcessName);
                 long wsSum = 0;
                 long wsMax = 0;
                 if (ps.Count() > 0)
@@ -50,7 +42,7 @@ namespace IEMemMon
                     wsMax = ps.Max(p => p.WorkingSet64) / scale;
                 }
                 // タスクトレイアイコンには文字数制限があるので注意
-                TaskTrayIcon.Text = $"Working Set({processName})\nSum: {wsSum.ToString("#,0")}MB\nMax: {wsMax.ToString("#,0")}MB";
+                TaskTrayIcon.Text = $"Working Set({ProcessName})\nSum: {wsSum.ToString("#,0")}MB\nMax: {wsMax.ToString("#,0")}MB";
 
                 if (alertMaxSize < wsMax)
                 {
@@ -63,6 +55,18 @@ namespace IEMemMon
             };
 
             counter.Start(TimeSpan.FromMilliseconds(1000));
+        }
+
+        private void setMenuItem()
+        {
+            ContextMenuStrip menu = new ContextMenuStrip();
+
+            ToolStripMenuItem closeItem = new ToolStripMenuItem();
+            closeItem.Text = "&終了";
+            closeItem.Click += CloseItem_Click;
+            menu.Items.Add(closeItem);
+
+            TaskTrayIcon.ContextMenuStrip = menu;
         }
 
         private void CloseItem_Click(object sender, EventArgs e)
